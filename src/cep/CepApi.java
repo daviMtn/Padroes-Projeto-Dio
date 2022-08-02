5100888e4db9126fd4daaec3c7104c8e5b4f8a15
Davi Martins <109256748+daviMtn@users.noreply.github.com>
2022-08-01 21:25:12 -0300

    Update CepApi.java

--- src/cep/CepApi.java
+++ src/cep/CepApi.java
@@ -13,10 +13,10 @@ public class CepApi {
 	}
 	
 	public String recuperarCidade(String cep) {
-		return "Araraquara";
+		return "Salvador";
 	}
 	
 	public String recuperarEstado(String cep) {
-		return "SP";
+		return "BA";
 	}
 }
